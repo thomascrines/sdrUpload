cfac9b514d82c50513cdce322a93a14c5cf29b5b
thomascrines <thomascrines@hotmail.com>
2019-08-26 11:39:58 +0100

    add upload.R and update description

---
+++ R/upload.R
@@ -0,0 +1,38 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+upload <- function(source) {
+
+  files <- list.files(source)
+
+  for (file in files) {
+
+    file_path <- file.path(source, file_name)
+    worksheets <- readxl::excel_sheets(file_path)
+
+    for (worksheet in worksheets) {
+
+      df <- readxl::read_excel(file_path, sheet = worksheet)
+      # TODO: Should sheet name be table name, or book/file name???
+      # TODO: Replace saveRDS with save to table
+      # saveRDS(df, file = paste0("test_save\\", worksheet, ".Rds"))
+
+      print("Format 1:")
+      print(str(df))
+      print("Format 2:")
+      print(sapply(df, typeof))
+    }
+  }
+}
